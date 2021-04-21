386649e7a6e1eee99e4a75bd6d83d8b245bd9c37
Jerome Haas <j.haas@cyberplanet.ch>
2021-04-21 13:28:49 +0200

    Implement: button component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { H1, H2, H3, H4, P } from 'components/text/Text';
 import { List } from 'components/text/List';
+import { Button } from 'components/links/Button';
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main>
-        Hello World
+        <Button target="/" color="red" buttonText="here we go" />
       </main>
 
     </div>
