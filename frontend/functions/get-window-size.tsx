f44d79a25b1fd518388633e8d8fc76af2ffc9706
Jerome Haas <j.haas@cyberplanet.ch>
2021-04-22 23:25:42 +0200

    Implement: setup bubble animation

--- frontend/functions/get-window-size.tsx
+++ frontend/functions/get-window-size.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 function useWindowSize() {
   const isSSR = typeof window === 'undefined';
   const [windowSize, setWindowSize] = React.useState({
-    width: isSSR ? 1200 : window.innerWidth,
-    height: isSSR ? 800 : window.innerHeight,
+    viewPortWidth: isSSR ? 1200 : window.innerWidth,
+    viewPortHeight: isSSR ? 800 : window.innerHeight,
   });
 
   function changeWindowSize() {
     setWindowSize({
-      width: window.innerWidth, height: window.innerHeight,
+      viewPortWidth: window.innerWidth, viewPortHeight: window.innerHeight,
     });
   }
 
