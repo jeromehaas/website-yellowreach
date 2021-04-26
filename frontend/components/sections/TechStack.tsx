1b09e51e75e3d6bdb9147a199999cfbeeafc69ec
Jerome Haas <j.haas@cyberplanet.ch>
2021-04-26 19:22:11 +0200

    Fix: remove links in the tech stack section

--- frontend/components/sections/TechStack.tsx
+++ frontend/components/sections/TechStack.tsx
@@ -10,12 +10,12 @@ const TechStack: React.FunctionComponent<Props> = () => (
   <div id="techstack" className="section">
     <div className="section__wrapper techstack__wrapper">
       <div className="techstack__box techstack__box--right background-color-yellow">
-        <Link target="/"><img className="techstack__image coresender" src="logos/tech-stack/coresender.png" alt="Coresender" /></Link>
-        <Link target="/"><img className="techstack__image ubuntu" src="logos/tech-stack/ubuntu.png" alt="Ubuntu" /></Link>
-        <Link target="/"><img className="techstack__image zurb" src="logos/tech-stack/zurb.png" alt="Zurb" /></Link>
-        <Link target="/"><img className="techstack__image nodemailer" src="logos/tech-stack/nodemailer.png" alt="Nodemailer" /></Link>
-        <Link target="/"><img className="techstack__image express" src="logos/tech-stack/express.png" alt="Express" /></Link>
-        <Link target="/"><img className="techstack__image nodejs" src="logos/tech-stack/nodejs.png" alt="NodeJS" /></Link>
+        <img className="techstack__image coresender" src="logos/tech-stack/coresender.png" alt="Coresender" />
+        <img className="techstack__image ubuntu" src="logos/tech-stack/ubuntu.png" alt="Ubuntu" />
+        <img className="techstack__image zurb" src="logos/tech-stack/zurb.png" alt="Zurb" />
+        <img className="techstack__image nodemailer" src="logos/tech-stack/nodemailer.png" alt="Nodemailer" />
+        <img className="techstack__image express" src="logos/tech-stack/express.png" alt="Express" />
+        <img className="techstack__image nodejs" src="logos/tech-stack/nodejs.png" alt="NodeJS" />
       </div>
       <div className="techstack__box techstack__box--left">
         <H3 className="techstack__title" textAlign="center">Tech Stack and Services</H3>
