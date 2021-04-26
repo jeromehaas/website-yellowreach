e5c78e96fc871a8b3681c56080050eef3534b671
Jerome Haas <j.haas@cyberplanet.ch>
2021-04-26 19:37:01 +0200

    Refactor: preload images of index page

--- frontend/components/partials/Head.tsx
+++ frontend/components/partials/Head.tsx
@@ -12,6 +12,20 @@ const Head: React.FunctionComponent<Props> = ({ title }) => (
     <meta name="viewport" content="initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no" />
     <meta name="author" content="Jérôme Haas" />
     <link rel="icon" href="favicon/favicon.png" />
+    <link rel="preload" as="image" href="/animations/sourcecode/sourcecode-ring-inner.png" />
+    <link rel="preload" as="image" href="/animations/sourcecode/sourcecode-ring-middle.png" />
+    <link rel="preload" as="image" href="/animations/sourcecode/sourcecode-ring-outer.png" />
+    <link rel="preload" as="image" href="/animations/sourcecode/sourcecode-static.png" />
+    <link rel="preload" as="image" href="/cursor/arrow.svg" />
+    <link rel="preload" as="image" href="/graphics/circle-grey-topleft.svg" />
+    <link rel="preload" as="image" href="/graphics/circle-grey.svg" />
+    <link rel="preload" as="image" href="/graphics/circle-white.svg" />
+    <link rel="preload" as="image" href="/images/code.png" />
+    <link rel="preload" as="image" href="/images/developer-team.jpeg" />
+    <link rel="preload" as="image" href="/images/developer.jpeg" />
+    <link rel="preload" as="image" href="/images/kappelbridge.png" />
+    <link rel="preload" as="image" href="/images/laptop.jpeg" />
+    <link rel="preload" as="image" href="/images/lucerne.jpeg" />
   </NextHead>
 );
 
